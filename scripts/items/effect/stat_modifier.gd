f61feaf20d4313a5baf3a354c9e90832c80e7470
Josh <josh@hartley.best>
2026-04-03 13:31:22 +0100

    SH-41 feat: inital effect resources

---
+++ scripts/items/effect/stat_modifier.gd
@@ -0,0 +1,13 @@
+class_name StatModifier
+extends RefCounted
+
+enum Operation { ADD, MULTIPLY }
+
+# Where it it come from?
+var source_key: String
+# What stat is being modified?
+var stat_key: StringName
+# How is the stat being modified?
+var operation: Operation
+# With what value is the stat modified?
+var value: float
