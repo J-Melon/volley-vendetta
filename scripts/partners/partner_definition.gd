48f84d1dd528ab41d48c08337cd94494ed76cf28
Josh <josh@hartley.best>
2026-04-12 23:43:04 +0100

    fix: removed type removal

--- scripts/partners/partner_definition.gd
+++ scripts/partners/partner_definition.gd
@@ -1,8 +1,10 @@
 class_name PartnerDefinition
 extends Resource
 
+const EffectType = preload("res://scripts/items/effect/effect.gd")
+
 @export var key: StringName
 @export var display_name: String
 @export var unlock_threshold: int
 @export var unlock_cost: int
-@export var effects: Array[Resource]
+@export var effects: Array[EffectType]
