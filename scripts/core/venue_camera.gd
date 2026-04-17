f520bc41737b6e2a80067f3c140796c2db6e7886
Josh <josh@hartley.best>
2026-04-17 14:52:49 +0100

    SH-106 feat: added rack placeholders

--- scripts/core/venue_camera.gd
+++ scripts/core/venue_camera.gd
@@ -1,12 +1,19 @@
 class_name VenueCamera
 extends Camera2D
 
-# todo: sh-106 clamp to venue bounds once shop and workshop placeholders exist.
-
 @export var pan_speed: float = 800.0
+@export var left_anchor: Node2D
+@export var right_anchor: Node2D
 
 
 func _process(delta: float) -> void:
 	var direction: float = Input.get_axis(&"camera_left", &"camera_right")
 	if direction != 0.0:
 		position.x += direction * pan_speed * delta
+	_clamp_to_anchors()
+
+
+func _clamp_to_anchors() -> void:
+	if left_anchor == null or right_anchor == null:
+		return
+	position.x = clamp(position.x, left_anchor.global_position.x, right_anchor.global_position.x)
